a809cb1c4af90ba346b879dfeccec1bfb8a46c24
Hamza <hazmasarwari426@gmail.com2>
2025-04-04 19:01:39 +0100

    Filled in the horse class

--- Horse.java
+++ Horse.java
@@ -7,8 +7,11 @@
  */
 public class Horse
 {
-    //Fields of class Horse
-    
+    private String horseName;
+    private char horseSymbol;
+    private int distanceTravelled;
+    private boolean fallen;
+    private double confidenceRating;
     
       
     //Constructor of class Horse
@@ -17,7 +20,10 @@ public class Horse
      */
     public Horse(char horseSymbol, String horseName, double horseConfidence)
     {
-       
+        this.horseSymbol = horseSymbol;
+        this.horseName = horseName;
+        this.confidenceRating = horseConfidence;
+
     }
     
     
@@ -25,52 +31,72 @@ public class Horse
     //Other methods of class Horse
     public void fall()
     {
+        if(this.fallen==false)
+        {
+            this.fallen = true;
+        }
+        else{
+            System.out.println(this.horseName +" has already fallen");
+        }
         
     }
     
     public double getConfidence()
     {
+        return this.confidenceRating;
         
     }
     
     public int getDistanceTravelled()
     {
-        
+        return  this.distanceTravelled;
     }
     
     public String getName()
     {
-        
+        return this.horseName;
     }
     
     public char getSymbol()
     {
-        
+        return this.horseSymbol;
     }
     
     public void goBackToStart()
     {
-        
+        this.distanceTravelled=0;
     }
     
     public boolean hasFallen()
     {
-        
+        return this.fallen;
     }
 
     public void moveForward()
     {
-        
+        this.distanceTravelled = this.distanceTravelled +1;
     }
 
     public void setConfidence(double newConfidence)
     {
-        
+        if(newConfidence<=0 &newConfidence<=1)
+        {
+            this.confidenceRating=newConfidence;
+        }
+        else{
+            System.out.println("Please enter a number between 0 and 1");
+        }
     }
     
     public void setSymbol(char newSymbol)
     {
-        
+        if(newSymbol==' ')
+        {
+            System.out.println("Please enter a valid character");
+        }
+        else{
+            this.horseSymbol=newSymbol;
+        }
     }
     
 }
